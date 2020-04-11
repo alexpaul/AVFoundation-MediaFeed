6a9cce793b5d7614dae65322f5427dfe059e458e
alexpaul <alexpaulnyc@gmail.com>
2020-04-11 17:04:43 -0400

    added collection view

---
+++ AVFoundation-MediaFeed/MediaFeedViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Alex Paul on 4/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class MediaFeedViewController: UIViewController {
+
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  @IBOutlet weak var videoButton: UIBarButtonItem!
+  @IBOutlet weak var photoLibraryButton: UIBarButtonItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    collectionView.dataSource = self
+    collectionView.delegate = self
+  }
+  
+  @IBAction func videoButtonPressed(_ sender: UIBarButtonItem) {
+    
+  }
+  
+  
+  @IBAction func photoLibraryButtonPressed(_ sender: UIBarButtonItem) {
+    
+  }
+}
+
+extension MediaFeedViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mediaCell", for: indexPath)
+    return cell
+  }
+}
+
+extension MediaFeedViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let maxSize: CGSize = UIScreen.main.bounds.size
+    let itemWidth: CGFloat = maxSize.width * 0.80
+    let itemHeight: CGFloat = itemWidth
+    return CGSize(width: itemWidth, height: itemHeight)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+  }
+}
+
