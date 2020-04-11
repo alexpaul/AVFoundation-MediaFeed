3cb7ae7e4671c47e134868a4917c298a69af070c
alexpaul <alexpaulnyc@gmail.com>
2020-04-11 16:15:53 -0400

    initial commit

---
+++ AVFoundation-MediaFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Alex Paul on 4/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
